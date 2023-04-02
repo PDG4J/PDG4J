6ac8336c82088a554cc882cb9f006b301a7d837f
everzakov <eaverzakov@edu.hse.ru>
2023-04-02 22:07:27 +0300

    add data flow graph

--- src/main/java/ru/hse/pdg4j/impl/task/graph/dfg/DataFlowGraphTask.java
+++ src/main/java/ru/hse/pdg4j/impl/task/graph/dfg/DataFlowGraphTask.java
@@ -5,12 +5,15 @@ import fr.inria.controlflow.ControlFlowEdge;
 import fr.inria.controlflow.ControlFlowGraph;
 import fr.inria.controlflow.ControlFlowNode;
 import fr.inria.dataflow.InitializedVariables;
+import java.util.HashMap;
 import org.codehaus.plexus.util.CollectionUtils;
 import ru.hse.pdg4j.api.PipelineContext;
 import ru.hse.pdg4j.api.PipelineTask;
 import ru.hse.pdg4j.api.PipelineTaskContext;
 import ru.hse.pdg4j.api.PipelineTaskResult;
 import ru.hse.pdg4j.impl.task.graph.cfg.ControlFlowGraphTask;
+import ru.hse.pdg4j.impl.task.graph.pdtg.ConditionalGraph;
+import ru.hse.pdg4j.impl.task.graph.pdtg.PreprocessControlFlowTask;
 import spoon.reflect.code.CtComment;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.factory.Factory;
@@ -24,49 +27,48 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static ru.hse.pdg4j.impl.SimplePipelineTaskResult.success;
 
 public class DataFlowGraphTask implements PipelineTask<DataFlowGraphTask.Context> {
-    private final AtomicInteger counter = new AtomicInteger(0);
+    public record Context(Map<CtMethod<?>, ConditionalGraph> graphMap) implements PipelineTaskContext {
+    }
+
+    public DataFlowGraphTask(String methodName) {
+        this.methodName = methodName;
+    }
+
+    private String methodName;
 
     @Override
     public String getName() {
         return "Build Data Flow Graph";
     }
 
+    private DataFlowGraphTask.Context context;
+
     @Override
     public DataFlowGraphTask.Context getContext() {
-        return new Context(null);
+        return context;
     }
 
     @Override
     public PipelineTaskResult run(PipelineContext context) {
-        var graph = context.getContext(ControlFlowGraphTask.Context.class).graphMap();
-        graph.forEach((ctMethod, controlFlowGraph) -> {
-            controlFlowGraph.simplify();
-            for (ControlFlowEdge controlFlowEdge : controlFlowGraph.edgeSet()) {
-                ControlFlowGraph newCfg = new ControlFlowGraph();
+        Map<CtMethod<?>, ConditionalGraph> graphMap = new HashMap<>();
 
-                ControlFlowNode begin = new ControlFlowNode(makeComment(ctMethod.getFactory()), newCfg, BranchKind.BEGIN);
-                ControlFlowNode source = new ControlFlowNode(controlFlowEdge.getSourceNode().getStatement(), newCfg, controlFlowEdge.getSourceNode().getKind());
-                ControlFlowNode target = new ControlFlowNode(controlFlowEdge.getTargetNode().getStatement(), newCfg, controlFlowEdge.getTargetNode().getKind());
-                ControlFlowNode exit = new ControlFlowNode(makeComment(ctMethod.getFactory()), newCfg, BranchKind.EXIT);
-
-                newCfg.addEdge(begin, source);
-                newCfg.addEdge(source, target);
-                newCfg.addEdge(target, exit);
-
-                newCfg.simplify();
-
-                InitializedVariables newVariables = new InitializedVariables();
-                newVariables.setIncludeDefinedInNode(false);
-                newVariables.run(newCfg.getExitNode());
-                System.out.println(source.getStatement() + " -> " + target.getStatement() + ": " + CollectionUtils.subtract(newVariables.getDefined(), newVariables.getUsed()));
+        var graph = context.getContext(PreprocessControlFlowTask.Context.class).graphMap();
+        graph.forEach((ctMethod, condtionalGraph) -> {
+            if (!ctMethod.getSimpleName().equals(methodName)) {
+                return;
             }
+            var newGraph = new DataFlowGraph(condtionalGraph);
+
+            graphMap.put(ctMethod, newGraph.getControlDependenceGraph());
         });
+
+        this.context = new Context(graphMap);
         return success();
     }
 
     @Override
     public Collection<Class<? extends PipelineTask<?>>> getRequirements() {
-        return List.of(ControlFlowGraphTask.class);
+        return List.of(PreprocessControlFlowTask.class);
     }
 
     private CtCommentImpl makeComment(Factory factory) {
